bc26c0f70b32b953daed290165432af1efb4c910
Giorgi <giogurabanidze@outlook.com>
2023-06-10 11:43:52 -0700

    added GeneralUtil and connect database

--- src/main/java/ibsu/project/taskmanagementsystem/controllers/UserController.java
+++ src/main/java/ibsu/project/taskmanagementsystem/controllers/UserController.java
@@ -2,6 +2,9 @@ package ibsu.project.taskmanagementsystem.controllers;
 
 import ibsu.project.taskmanagementsystem.dto.AddUser;
 import ibsu.project.taskmanagementsystem.entities.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,4 +15,15 @@ public class UserController {
 
         return null;
     }
+    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<User> getUser(@PathVariable String user){
+//        User testUser = new User();
+//        testUser.setPassword("0111022");
+//        testUser.setEmail("girogi@g.2");
+//        testUser.setFirstName("giorgi");
+//        testUser.setLastName("gurabana");
+//        testUser.getUserId();
+        return  new ResponseEntity<User>(HttpStatus.OK);
+    }
 }
+
