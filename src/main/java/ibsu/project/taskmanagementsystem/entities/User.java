87ef55f9a13542ffb3d0282e769620ad1b94ca2d
Giorgi <giogurabanidze@outlook.com>
2023-06-12 15:12:03 -0700

    added Repository and UserService

--- src/main/java/ibsu/project/taskmanagementsystem/entities/User.java
+++ src/main/java/ibsu/project/taskmanagementsystem/entities/User.java
@@ -1,6 +1,59 @@
 package ibsu.project.taskmanagementsystem.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+
+    private Long Id;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    private String email;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
